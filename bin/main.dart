3ba11251fd28485efe1212eb5fe2c1be25edf63c
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-23 17:03:54 -0400

    rearrange file structure

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
+import 'package:test/cart.dart';
+import 'package:test/product.dart';
+
 const allProducts = [
   Product(id: 1, name: "apples", price: 1.50),
   Product(id: 2, name: "bananas", price: 2.25),
@@ -8,57 +11,6 @@ const allProducts = [
   Product(id: 5, name: "cucumbers", price: 2.50),
   Product(id: 6, name: "steak", price: 19.50),
 ];
-
-class Product {
-  const Product({required this.id, required this.name, required this.price});
-  final int id;
-  final String name;
-  final double price;
-
-  String get displayName => '($initial)${name.substring(1)}: \$$price';
-  String get initial => name.substring(0, 1);
-}
-
-class Item {
-  const Item({required this.product, this.quantity = 1});
-
-  final Product product;
-  final int quantity;
-
-  double get price => quantity * product.price;
-
-  @override
-  String toString() => '$quantity x ${product.name}: \$$price';
-}
-
-class Cart {
-  final Map<int, Item> _items = {};
-
-  void addProduct(product) {
-    final item = _items[product.id];
-    if (item == null) {
-      _items[product.id] = Item(product: product, quantity: 1);
-    } else {
-      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
-    }
-  }
-
-  double total() => _items.values
-      .map((item) => item.price)
-      .reduce((value, element) => value + element);
-
-  @override
-  String toString() {
-    if (_items.isEmpty) {
-      print('cart be empty bruv');
-    }
-    final itemizedList =
-        _items.values.map((item) => item.toString()).join('\n');
-    return '------\n$itemizedList\nTotal: \$${total()}\n------';
-  }
-
-  bool get isEmpty => _items.isEmpty;
-}
 
 Product? chooseProduct() {
   final productList =
---
+++ lib/cart.dart
@@ -0,0 +1,31 @@
+import 'item.dart';
+import 'product.dart';
+
+class Cart {
+  final Map<int, Item> _items = {};
+
+  void addProduct(Product product) {
+    final item = _items[product.id];
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _items.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  @override
+  String toString() {
+    if (_items.isEmpty) {
+      print('cart be empty bruv');
+    }
+    final itemizedList =
+        _items.values.map((item) => item.toString()).join('\n');
+    return '------\n$itemizedList\nTotal: \$${total()}\n------';
+  }
+
+  bool get isEmpty => _items.isEmpty;
+}
